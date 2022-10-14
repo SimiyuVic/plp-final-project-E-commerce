91f5fdfe5535b102961d46dc275b36b6279807f4
aoko-code <cynthiakhamenge@yahoo.com>
2022-10-14 14:43:55 +0300

    products

--- plp/index.php
+++ plp/index.php
@@ -22,85 +22,95 @@
      ?>
   		</div>
   	</div>
-  </div>  
-  <div class="featured-items">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <div class="line-dec"></div>
-              <h1>Featured Items</h1>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <div class="owl-carousel owl-theme">
-              <a href="single-product.html">
-                <div class="featured-item">
-                  <img src="assets/images/item-01.jpg" alt="Item 1">
-                  <h4>Proin vel ligula</h4>
-                  <h6>$15.00</h6>
-                </div>
-              </a>
-              <a href="single-product.html">
-                <div class="featured-item">
-                  <img src="assets/images/item-02.jpg" alt="Item 2">
-                  <h4>Erat odio rhoncus</h4>
-                  <h6>$25.00</h6>
-                </div>
-              </a>
-              <a href="single-product.html">
-                <div class="featured-item">
-                  <img src="assets/images/item-03.jpg" alt="Item 3">
-                  <h4>Integer vel turpis</h4>
-                  <h6>$35.00</h6>
-                </div>
-              </a>
-              <a href="single-product.html">
-                <div class="featured-item">
-                  <img src="assets/images/item-04.jpg" alt="Item 4">
-                  <h4>Sed purus quam</h4>
-                  <h6>$45.00</h6>
-                </div>
-              </a>
-              <a href="single-product.html">
-                <div class="featured-item">
-                  <img src="assets/images/item-05.jpg" alt="Item 5">
-                  <h4>Morbi aliquet</h4>
-                  <h6>$55.00</h6>
-                </div>
-              </a>
-              <a href="single-product.html">
-                <div class="featured-item">
-                  <img src="assets/images/item-06.jpg" alt="Item 6">
-                  <h4>Urna ac diam</h4>
-                  <h6>$65.00</h6>
-                </div>
-              </a>
-              <a href="single-product.html">
-                <div class="featured-item">
-                  <img src="assets/images/item-04.jpg" alt="Item 7">
-                  <h4>Proin eget imperdiet</h4>
-                  <h6>$75.00</h6>
-                </div>
-              </a>
-              <a href="single-product.html">
-                <div class="featured-item">
-                  <img src="assets/images/item-05.jpg" alt="Item 8">
-                  <h4>Nullam risus nisl</h4>
-                  <h6>$85.00</h6>
-                </div>
-              </a>
-              <a href="single-product.html">
-                <div class="featured-item">
-                  <img src="assets/images/item-06.jpg" alt="Item 9">
-                  <h4>Cras tempus</h4>
-                  <h6>$95.00</h6>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+  </div>
+    
+  <?php
+
+
+    if(isset($_GET['category']))
+        {
+
+            $category_slug = $_GET['category'];
+            $category_data = getSlugActive("categories",$category_slug);
+            $category = mysqli_fetch_array($category_data);
+
+            if($category)
+            {
+                $category_id = $category['id'];
+
+                 ?>
+
+                   <div class="py-3 bg-primary">
+                       <div class="container">
+                           <h6 class="text-white">
+                               <a class="text-white" href="categories.php">
+                                   Home /
+                               </a>
+                               <a class="text-white" href="categories.php">
+                                   Collections /
+                               </a>
+                            
+                             <?= $category['name']; ?></h6>
+                       </div>
+                   </div>
+                   <div class="py-3">
+                       <div class="container">
+                           <div class="row">
+                               <div class="col-md-12">
+                                   <h2><?= $category['name']; ?></h2>
+                                   <hr>
+                                   <div class="row">
+
+                                       <?php
+                                           $products = getProdByCategory($category_id);
+
+                                           if(mysqli_num_rows($products) > 0)
+                                           {
+                                               foreach($products as $item)
+                                               {
+                                                   ?>
+                                                   <div class="col-md-3 mb-2">
+                                                      <a href="view-product.php?product=<?= $item['slug']; ?>">
+                                                         <div class="card shadow">
+                                                           <div class="card-body">
+                                                               <img src="uploads/<?= $item['image'];?>" alt="Product Image" class="w-100 height-50px"  >
+                                                               <h4 class="text-center"><?= $item['name'];?></h4>
+                                                           </div>
+                                                         </div>
+                                                      </a>
+                                                   </div>
+                                                       
+                                                   <?php
+                                               }
+                                           }
+                                           else
+                                           {
+                                               echo "No data available ";
+                                           }
+                                       ?>
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+
+               <?php
+            }
+            else
+             {
+               echo "Something went wrong";
+             }
+
+            }
+             else
+             {
+               echo "Something went wrong";
+             }
+
+ 
+
+   ?>
+
     </div>
 
 <?php include('includes/footer.php'); ?>
